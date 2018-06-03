874b5acf4225b586a7412e00ade4214682a3274f
CJianYu <externalanswer@gmail.com>
2018-06-03 22:23:52 +0800

    change to react

--- README.md
+++
@@ -1,2 +0,0 @@
-# webnotes
-My personal site
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {
+  Component
+} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="home">
+            <header>
+                <h1><a href="#/" className="blog_title">懒人的梦呓</a></h1>
+            </header>
+            <div className="container">
+
+            </div>
+      </div>
+    );
+  }
+}
+
+export default App;
